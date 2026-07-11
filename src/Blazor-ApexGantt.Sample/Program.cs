0f93e77c8be19abe7853bcce8a02ed1944cea989
Juned Chhipa <juned.chhipa@gmail.com>
2026-07-11 23:08:06 +0530

    demo: apply domain-locked license key on GitHub Pages demo (removes watermark)

--- src/Blazor-ApexGantt.Sample/Program.cs
+++ src/Blazor-ApexGantt.Sample/Program.cs
@@ -9,12 +9,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddApexGantt();
-
-// configure license if key available
-// builder.Services.AddApexGantt(options =>
-// {
-//     options.LicenseKey = "your-license-key-here";
-// });
+// license for the public demo hosted at https://apexcharts.github.io/ (domain-locked, no watermark)
+builder.Services.AddApexGantt(options =>
+{
+    options.LicenseKey = "APEX-eyJpc3N1ZURhdGUiOiIyMDI2LTA3LTA3IiwiZXhwaXJ5RGF0ZSI6IjIwNTAtMDctMDciLCJwbGFuIjoiZW50ZXJwcmlzZSIsImRvbWFpbnMiOlsiYXBleGNoYXJ0cy5naXRodWIuaW8iXX0=";
+});
 
 await builder.Build().RunAsync();
